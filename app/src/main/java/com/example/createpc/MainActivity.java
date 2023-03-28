b5b841d036074906502483e9bf3b146caec47322
Evgeny <genyakarelin23@gmail.com>
2023-03-28 20:33:55 +0300

    Added view binding implementation

--- app/src/main/java/com/example/createpc/MainActivity.java
+++ app/src/main/java/com/example/createpc/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.createpc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.createpc.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = activityMainBinding.getRoot();
+        setContentView(view);
     }
 }
