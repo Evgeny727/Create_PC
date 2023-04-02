723d02d24c4e2198e4337e390f770f4982f0a7a2
Evgeny <genyakarelin23@gmail.com>
2023-04-02 18:00:01 +0300

    Added bottom navigation menu and 2 destinations

--- app/src/main/java/com/example/createpc/MainActivity.java
+++ app/src/main/java/com/example/createpc/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.createpc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.createpc.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +23,28 @@ public class MainActivity extends AppCompatActivity {
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = activityMainBinding.getRoot();
         setContentView(view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView = activityMainBinding.navigationMenu;
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.page_create:
+                        item.setChecked(true);
+                        navController.navigate(R.id.startFragment);
+                        break;
+                    case R.id.page_search:
+                        item.setChecked(true);
+                        navController.navigate(R.id.searchFragment);
+                        break;
+                    case R.id.page_builds:
+                        item.setChecked(true);
+                        navController.navigate(R.id.buildsFragment);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
